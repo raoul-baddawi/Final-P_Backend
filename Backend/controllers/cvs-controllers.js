16e0872dfe1f61b919789c9867a2d3004fe87302
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-05-23 22:35:10 +0300

    Adjusted keys

--- Backend/controllers/cvs-controllers.js
+++ Backend/controllers/cvs-controllers.js
@@ -6,7 +6,7 @@ const Cv = require("../models/cv");
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: API_SECRET,
+  api_secret: process.env.API_SECRET,
 });
 
 // this function get the links
--- Backend/controllers/profiles-controllers.js
+++ Backend/controllers/profiles-controllers.js
@@ -7,7 +7,7 @@ const Profile = require("../models/profiles");
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: API_SECRET,
+  api_secret: process.env.API_SECRET,
 });
 
 
